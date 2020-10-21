e47a1be69d711fcf2ed6b1daf11fe66c195492f0
teng <wwwsousoucom@126.com>
2020-10-21 18:22:01 +0800

    矩阵

--- linear_algebra/playLA/Matrix.py
+++ linear_algebra/playLA/Matrix.py
@@ -5,6 +5,57 @@ class Matrix:
     def __init__(self, list2d):
         self._values = [row[:] for row in list2d]
 
+    @classmethod
+    def zero(cls, r, c):
+        """返回一个r行c列的零矩阵"""
+        return cls([[0] * c for _ in range(r)])
+
+    def __add__(self, another):
+        """
+        返回两个矩阵的加法结果
+        :param another:
+        :return:
+        """
+        assert self.shape() == another.shape(), \
+            "Error in adding. Shape of matrix must be same."
+        return Matrix([[a + b for a, b in zip(self.row_vector(i), another.row_vector(i))]
+                       for i in range(self.row_num())])
+
+    def __sub__(self, another):
+        """
+        返回两个矩阵的加法结果
+        :param another:
+        :return:
+        """
+        assert self.shape() == another.shape(), \
+            "Error in subtracting. Shape of matrix must be same."
+        return Matrix([[a - b for a, b in zip(self.row_vector(i), another.row_vector(i))]
+                       for i in range(self.row_num())])
+
+    def __mul__(self, k):
+        """返回矩阵的数量乘法 self * k"""
+        return Matrix([[e * k for e in self.row_vector(i)]
+                       for i in range(self.row_num())])
+
+    def __rmul__(self, k):
+        return self * k
+
+    def __truediv__(self, k):
+        """
+        返回数量除法的结果矩阵: self/k
+        :param k:
+        :return:
+        """
+        return (1/k) * self
+
+    def __pos__(self):
+        """返回矩阵取正的结果"""
+        return 1 * self
+
+    def __neg__(self):
+        """返回矩阵取负的结果"""
+        return -1 * self
+
     def row_vector(self, index):
         """返回矩阵的第index个行向量"""
         return Vector(self._values[index])
